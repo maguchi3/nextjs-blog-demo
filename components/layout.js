7ddd9b50c2d8ab2b1fe8228c819ec5a70e40e44e
山口遼大 <ryamaguchi@yamaguchiryoudainoMacBook-Air.local>
2021-03-17 09:26:17 +0900

    profile-path changed

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile-2.jpeg"
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
